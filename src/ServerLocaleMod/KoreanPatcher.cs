06a0327fca8c37b0232214b6a45123a7861d53cf
GoLani11 <gomim88@naver.com>
2026-06-20 22:34:21 +0900

    서버 로케일 모드 버전 1.5.0으로 갱신

--- src/ServerLocaleMod/KoreanPatcher.cs
+++ src/ServerLocaleMod/KoreanPatcher.cs
@@ -13,7 +13,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "SPT_Korean_Localization_(G&M)";
     public override string Author { get; init; } = "Golani, Makina";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("1.4.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("1.5.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; } = null;
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = null;
